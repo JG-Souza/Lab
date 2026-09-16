5e87cc7304acace2bd650a21c43b027e697dc0ee
JG-Souza <joaogsouzafma@gmail.com>
2026-09-16 19:54:09 -0300

    possibilita rodar o projeto local e em container

--- src/server.js
+++ src/server.js
@@ -1,2 +1,4 @@
 const app = require('./app');
-app.listen(3000, () => console.log('listening on 3000'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on ${port}`));
